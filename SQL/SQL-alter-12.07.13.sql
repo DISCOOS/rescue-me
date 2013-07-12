e2430df3b99feafdc772bd2166cf9587f1e5a777
Sven-Ove <Sven-Ove@Sven-Ove-PC>
2013-07-12 17:05:07 +0200

    SMS-delivery is now logged (Sveve). TODO: Show in GUI

--- SQL/SQL-alter-12.07.13.sql
+++ SQL/SQL-alter-12.07.13.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `missing` ADD `sms_mb_sent` ENUM( 'false', 'true' ) NOT NULL 
+ADD `sms_delivered` ENUM( 'false', 'true' ) NOT NULL AFTER `sms_sent` ,
+ADD `sms_error` VARCHAR( 255 ) NOT NULL AFTER `sms_delivered`,
+ADD `sms_provider_ref` VARCHAR( 255 ) NOT NULL AFTER `sms_delivered` 
