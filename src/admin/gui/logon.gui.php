812ce2c642332388e262d83b0cb6824fc8cb9e8f
Kenneth Gulbrandsøy <kenneth@discoos.org>
2013-07-18 01:47:44 +0200

    Fixed validation

--- src/admin/gui/logon.gui.php
+++ src/admin/gui/logon.gui.php
@@ -1,19 +1,12 @@
-<form class="form-signin" method="post" action="<?= ADMIN_URI."logon" ?>">
+<form id="logon" name="logon" class="form-signin" method="post" action="<?= ADMIN_URI."logon" ?>">
     <h2 class="form-signin-heading">Logg inn</h2>
-    <?php
-        if(isset($_ROUTER['message'])) { 
-    ?>
-        <div class="alert alert-error"><?= $_ROUTER['message'] ?></div>
-        <input type="email" name="username" class="input-block-level alert-error" placeholder="E-postadresse">
-        <input type="password" name="password" class="input-block-level alert-error" placeholder="Passord">
-    <?
-        } else {
-    ?>    
-        <input type="email" name="username" class="input-block-level" placeholder="E-postadresse"></input>
-        <input type="password" name="password" id="password" class="input-block-level" placeholder="Passord" data-content="Cap-lock is on!"></input>
-    <?
-        }
-    ?>    
+<?php 
+    if(isset($_ROUTER['message'])) { 
+        insert_error($_ROUTER['message']);
+    }  
+?>    
+    <input name="username" type="email" class="input-block-level" placeholder="E-postadresse" required></input>
+    <input name="password" type="password" class="input-block-level" placeholder="Passord" data-content="Caps-lock is on!" required></input>
     <label class="checkbox">
         <input type="checkbox" value="remember-me"> Husk meg på denne maskinen
     </label>
