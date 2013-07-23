dc2a72c1f3ab1bc71a808cf5179568ce2e81ed38
Kenneth Gulbrandsøy <kenneth@discoos.org>
2013-07-23 22:42:43 +0200

    Added autofocus on first field

--- src/admin/gui/logon.gui.php
+++ src/admin/gui/logon.gui.php
@@ -5,7 +5,7 @@
         insert_error($_ROUTER['message']);
     }  
 ?>    
-    <input name="username" type="email" class="input-block-level" placeholder="E-postadresse" required></input>
+    <input name="username" type="email" class="input-block-level" placeholder="E-postadresse" required autofocus></input>
     <input name="password" type="password" class="input-block-level" placeholder="Passord" data-content="Caps-lock is on!" required></input>
     <label class="checkbox">
         <input type="checkbox" value="remember-me"> Husk meg på denne maskinen
