95f2586633a773a59ed3dcf29cb2b98e647d30bd
Kenneth Gulbrandsøy <kenneth@discoos.org>
2013-07-30 20:19:06 +0200

    Removed 'remember me'

--- src/admin/gui/logon.gui.php
+++ src/admin/gui/logon.gui.php
@@ -7,8 +7,5 @@
 ?>    
     <input name="username" type="email" class="input-block-level" placeholder="E-postadresse" required autofocus></input>
     <input name="password" type="password" class="input-block-level" placeholder="Passord" data-content="Caps-lock is on!" required></input>
-    <label class="checkbox">
-        <input type="checkbox" value="remember-me"> Husk meg på denne maskinen
-    </label>
     <button class="btn btn-large btn-primary" type="submit">Logg inn</button>
 </form>
