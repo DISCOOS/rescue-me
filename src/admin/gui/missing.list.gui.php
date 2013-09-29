dfe9c29afbcbe253aa1d25d9e389793778d267fa
Kenneth Gulbrandsøy <kenneth@discoos.org>
2013-09-29 20:16:50 +0200

    Moved operations into tabes. Close #79.

--- src/admin/gui/missing.list.gui.php
+++ src/admin/gui/missing.list.gui.php
@@ -16,10 +16,18 @@
     
 ?>
 
-    <h3>Aktive operasjoner</h3>
-    
-    <? if($active == false) { insert_alert(_("Ingen registrert"));  } else { ?>
-    
+<h3>Operasjoner</h3>
+
+<ul id="tabs" class="nav nav-tabs">
+  <li><a href="#active" data-toggle="tab"><?=_("Aktive")?></a></li>
+  <li><a href="#closed" data-toggle="tab"><?=_("Lukkede")?></a></li>
+</ul>
+
+<div class="tab-content" style="width: auto; overflow: visible">
+    <div id="active" class="tab-pane active">
+
+<? if($active == false) { insert_alert(_("Ingen registrert"));  } else { ?>
+
     <table class="table table-striped">
         <thead>
             <tr>
@@ -35,97 +43,96 @@
             </tr>
         </thead>        
         <tbody class="searchable">
-    <?
-        // Enable manual SMS delivery status check?
-        $module = Module::get("RescueMe\SMS\Provider", User::currentId());
-        $sms = $module->newInstance();
-        $check = ($sms instanceof RescueMe\SMS\Check);
-        foreach($active as $id => $this_operation) {
-            $missings = $this_operation->getAllMissing();
-            $this_missing = current($missings);
-            $resend[$this_missing->id] = $this_missing;
-            $this_missing->getPositions();
-            if($this_missing->last_pos->timestamp>-1) {
-                $position = $this_missing->last_UTM;
-                $received = format_since($this_missing->last_pos->timestamp);
-            } else {
-                $received = "";
-                $position = $this_missing->last_pos->human;
-            }
-            $sent = format_since($this_missing->sms_sent);
-            if($check && !isset($this_missing->sms_delivery) && $this_missing->sms_provider === $module->impl) {
-                $code = Locale::getDialCode($this_missing->mobile_country);
-                $code = $sms->accept($code);
-                $ref = $this_missing->sms_provider_ref;
-                if(!empty($ref) && $sms->request($ref,$code.$this_missing->mobile)) {
-                    $this_missing = Missing::getMissing($id);
-                }
-            }
-            $answered = format_since($this_missing->answered);
-            $delivered = format_since($this_missing->sms_delivery);
-            if (empty($delivered))
-                $delivered = _('Ukjent');
-            
-    ?>
-            <tr id="<?= $this_missing->id ?>">
-                <td class="missing name"> <?= $this_operation->op_name ?> </td>
-                <td class="missing sent hidden-phone"><?= $sent ?></td>
-                <td id="delivered-<?=$id?>" class="missing delivered hidden-phone"><?= $delivered ?></td>
-                <td id="responded-<?=$id?>" class="missing answered hidden-phone"><?= $answered ?></td>
-                <td class="missing received hidden-phone"><?= $received ?></td>
-                <td class="missing position"><?= $position ?></td>
-                <td class="missing editor">
-                    <div class="btn-group pull-right">
-                        <a class="btn btn-small" href="<?=ADMIN_URI."missing/edit/$this_missing->id"?>">
-                            <b class="icon icon-edit"></b><?= EDIT ?>
-                        </a>
-                        <a class="btn btn-small dropdown-toggle" data-toggle="dropdown">
-                            <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a role="menuitem" href="<?=ADMIN_URI."operation/close/$this_missing->id"?>">
-                                    <b class="icon icon-off"></b><?= _('Avslutt operasjon') ?>
-                                </a>
-                            </li>
-                            <li>
-                                <a role="menuitem" href="#confirm-resend-<?=$id?>" data-toggle="modal">
-                                    <b class="icon icon-envelope"></b><?= _('Send SMS på nytt') ?>
-                                </a>
-                            </li>                                
-                            <li class="divider"></li>
-                            <li>
-                                <a role="menuitem" onclick="R.ajax('<?=ADMIN_URI."missing/check/$id"?>','#delivered-<?=$id?>');">
-                                    <b class="icon icon-refresh"></b><?= _('Sjekk leveringsstatus') ?>
-                                </a>
-                           </li>   
-                        </ul>
-                    </div>
-                </td>
-            </tr>
-    <? }} ?>
-            
-        </tbody>
-    </table>
-        
-    <?  
-        if (!empty($resend)) {
-            foreach($resend as $id => $this_missing) {
-                // Insert resend confirmation
-                insert_dialog_confirm(
-                    "confirm-resend-$id", 
-                    "Bekreft", 
-                    _("Vil du sende SMS til <u>$this_missing->name</u> på nytt?"), 
-                    ADMIN_URI."missing/resend/{$id}"
-                );
+<?
+    // Enable manual SMS delivery status check?
+    $module = Module::get("RescueMe\SMS\Provider", User::currentId());
+    $sms = $module->newInstance();
+    $check = ($sms instanceof RescueMe\SMS\Check);
+    foreach($active as $id => $this_operation) {
+        $missings = $this_operation->getAllMissing();
+        $this_missing = current($missings);
+        $resend[$this_missing->id] = $this_missing;
+        $this_missing->getPositions();
+        if($this_missing->last_pos->timestamp>-1) {
+            $position = $this_missing->last_UTM;
+            $received = format_since($this_missing->last_pos->timestamp);
+        } else {
+            $received = "";
+            $position = $this_missing->last_pos->human;
+        }
+        $sent = format_since($this_missing->sms_sent);
+        if($check && !isset($this_missing->sms_delivery) && $this_missing->sms_provider === $module->impl) {
+            $code = Locale::getDialCode($this_missing->mobile_country);
+            $code = $sms->accept($code);
+            $ref = $this_missing->sms_provider_ref;
+            if(!empty($ref) && $sms->request($ref,$code.$this_missing->mobile)) {
+                $this_missing = Missing::getMissing($id);
             }
         }
-    ?>
+        $answered = format_since($this_missing->answered);
+        $delivered = format_since($this_missing->sms_delivery);
+        if (empty($delivered))
+            $delivered = _('Ukjent');
 
-    <h3>Avsluttede operasjoner</h3>
-    
-    <? if($closed == false) { insert_alert(_("Ingen registrert"));  } else { ?>
-    
+?>
+                <tr id="<?= $this_missing->id ?>">
+                    <td class="missing name"> <?= $this_operation->op_name ?> </td>
+                    <td class="missing sent hidden-phone"><?= $sent ?></td>
+                    <td id="delivered-<?=$id?>" class="missing delivered hidden-phone"><?= $delivered ?></td>
+                    <td id="responded-<?=$id?>" class="missing answered hidden-phone"><?= $answered ?></td>
+                    <td class="missing received hidden-phone"><?= $received ?></td>
+                    <td class="missing position"><?= $position ?></td>
+                    <td class="missing editor">
+                        <div class="btn-group pull-right">
+                            <a class="btn btn-small" href="<?=ADMIN_URI."missing/edit/$this_missing->id"?>">
+                                <b class="icon icon-edit"></b><?= EDIT ?>
+                            </a>
+                            <a class="btn btn-small dropdown-toggle" data-toggle="dropdown">
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a role="menuitem" href="<?=ADMIN_URI."operation/close/$this_missing->id"?>">
+                                        <b class="icon icon-off"></b><?= _('Avslutt operasjon') ?>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a role="menuitem" href="#confirm-resend-<?=$id?>" data-toggle="modal">
+                                        <b class="icon icon-envelope"></b><?= _('Send SMS på nytt') ?>
+                                    </a>
+                                </li>                                
+                                <li class="divider"></li>
+                                <li>
+                                    <a role="menuitem" onclick="R.ajax('<?=ADMIN_URI."missing/check/$id"?>','#delivered-<?=$id?>');">
+                                        <b class="icon icon-refresh"></b><?= _('Sjekk leveringsstatus') ?>
+                                    </a>
+                               </li>   
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+<? }} ?>
+            </tbody>
+        </table>
+<?  
+    if (!empty($resend)) {
+        foreach($resend as $id => $this_missing) {
+            // Insert resend confirmation
+            insert_dialog_confirm(
+                "confirm-resend-$id", 
+                "Bekreft", 
+                _("Vil du sende SMS til <u>$this_missing->name</u> på nytt?"), 
+                ADMIN_URI."missing/resend/{$id}"
+            );
+        }
+    }
+?>
+
+</div>
+<div id="closed" class="tab-pane">
+
+<? if($closed == false) { insert_alert(_("Ingen registrert"));  } else { ?>
+
     <table class="table table-striped">
         <thead>
             <tr>
@@ -135,10 +142,10 @@
             </tr>
         </thead>        
         <tbody class="searchable">
-    <?
-        foreach($closed as $id => $this_operation) {
-            
-    ?>
+<?
+    foreach($closed as $id => $this_operation) {
+
+?>
             <tr id="<?= $id ?>">
                 <td class="missing name"> <?= $this_operation->op_name ?> </td>
                 <td class="missing date"><?= format_dt($this_operation->op_closed) ?></td>
@@ -150,7 +157,13 @@
                     </div>
                 </td>
             </tr>
-    <? }} ?>
-            
+<? }} ?>
+
         </tbody>
     </table>
+</div>
+    
+<script>
+    R.toTab('tabs');
+</script>
+    
