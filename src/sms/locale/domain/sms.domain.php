ebaef7dab1263d1b1898e844720e0dac07b2e154
Kenneth Gulbrandsøy <kenneth@discoos.org>
2015-07-23 16:52:43 +0200

    Removed unused parameter from SMS position message

--- src/sms/locale/domain/sms.domain.php
+++ src/sms/locale/domain/sms.domain.php
@@ -6,14 +6,6 @@
 
         define('DOMAIN_SMS','sms');
 
-//        define('ALERT_SMS_TRACE', 'We are searching for you! Click on this link so we can locate you: %LINK%');
-//        define('ALERT_SMS_NOT_SENT', 'Warning: SMS not sent to #m_name');
-//        define('ALERT_SMS_2', 'If you have GPS on your phone, we recommend that you enable this. In most cases you will find this under Settings -> General, or Settings -> Location.');
-//        define('ALERT_SMS_LOCATION_UPDATE', 'Received location of #m_name: #pos (+/-#acc m)!' . ' '. ADMIN_TRACE_URL);
-
-
-
-
     }
 
     return $defined;
