52ca32e8cad74100b9b201b632c0db3965a4c52d
Kenneth Gulbrandsøy <kenneth@discoos.org>
2017-07-09 14:36:07 +0200

    Fixed logs not loading

--- src/admin/gui/logs.gui.php
+++ src/admin/gui/logs.gui.php
@@ -19,7 +19,7 @@
 
 <ul id="tabs" class="nav nav-tabs">
 <? foreach($titles as $name => $title) { $names[] = $name; ?>
-  <li><a href="<?=ADMIN_URI?>logs#<?=$name?>" data-toggle="tab"><?=$title?></a></li>
+  <li><a href="#<?=$name?>" data-toggle="tab"><?=$title?></a></li>
 <? } ?>  
 </ul>
 <div class="tab-content" style="width: auto; overflow: visible">       
